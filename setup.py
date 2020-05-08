b316cbf5bbebf9ef18b6ea279249ab23a8c6f5ed
Padamja <padamja.gupta@gmail.com>
2020-05-08 23:09:22 +0530

    renames project and removes unwanted adjectives

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ with fopen(join(DIR, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'gender_neutral_name',
+    name = 'gender_neutral_names',
     version = VERSION,
-    description = 'Faker provider for gender neutral name generator',
+    description = 'Faker provider for gender neutral names generator',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    keywords = 'faker faker-library faker-provider faker-generator gender-neutral name-generator',
+    keywords = 'faker faker-library faker-provider faker-generator gender-neutral names-generator',
     author = 'Padamja',
     author_email = 'padamja.gupta@gmail.com',
     license='MIT',
-    url = 'https://github.com/Padamja/gender_neutral_name',
-    download_url = 'https://github.com/Padamja/gender_neutral_name/archive/v{0}.tar.gz'.format(VERSION),
+    url = 'https://github.com/Padamja/gender_neutral_names',
+    download_url = 'https://github.com/Padamja/gender_neutral_names/archive/v{0}.tar.gz'.format(VERSION),
     zip_safe = True,
     classifiers = [
         'Development Status :: 4 - Beta',
@@ -31,7 +31,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2 :: Only',
     ],
-    packages = ['gender_neutral_name'],
+    packages = ['gender_neutral_names'],
     # package_data={'gender_neutral_name':['data/*.json']},
     install_requires = [
         'Faker'
